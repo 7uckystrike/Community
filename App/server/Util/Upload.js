238c94862c32d186a636c7b1f1c1ac9e76f960f4
yeoung <yeoung.dev@gmail.com>
2023-02-25 22:56:06 +0900

    🐛  fix

--- App/server/Util/Upload.js
+++ App/server/Util/Upload.js
@@ -32,27 +32,3 @@ function setUpload(bucket) {
 }
 
 module.exports = setUpload;
-
-
-/*
-const storage = multer.diskStorage({
-  destination : function (req, file, cd) {
-    cd(null, "image/");
-  },
-  filename: function (req, file, cb) {
-    cb(null, Date.now() + "-" + file.originalname);
-  }
-})
-
-const upload = multer({ storage : storage }).single("file");
-
-router.post("/image/upload", (req, res) => {
-  upload(req, res, (err) => {
-    if(err) {
-      res.status(400).json({ success : false });
-    } else {
-      res.status(200).json({ success : true, filePath : res.req.file.path });
-    }
-  })
-})
-*/
